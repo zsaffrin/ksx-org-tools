525c6082fda2127be774a6edbebb8b3c68d27ffc
Zach Saffrin <awesomezach@gmail.com>
2025-10-08 18:37:34 -0400

    Add legacy shortcut buttons

--- src/components/App/Pages/Pages.tsx
+++ src/components/App/Pages/Pages.tsx
@@ -1,30 +1,36 @@
-// import useTabData from '../../../hooks/useTabData';
+import useUrlParams from '../../../hooks/useUrlParams';
+import { openNewPage } from '../../../utilities';
 import './Pages.css';
 
 const Pages = () => {
-  // const { domain } = useTabData();
+  const params = useUrlParams();
 
-  // const openNewPage = (targetUrl: string) => {
-  //   window.open('https://' + domain + targetUrl);
-  // };
+  const targetList = [
+    { title: 'Job Administration', target: '/apex/KimbleOne__JobAdministration' },
+    { title: 'Apex Jobs', target: '/lightning/setup/AsyncApexJobs/home' },
+    { title: 'Scheduled Jobs', target: '/lightning/setup/ScheduledJobs/home' },
+    { title: 'Event Classes', target: '/lightning/o/KimbleOne__EventClass__c/home' },
+    { title: 'Reference Data', target: '/lightning/n/KimbleOne__ReferenceData' },
+    { title: 'Org-Wide Config', target: '/apex/KimbleOne__ConfigurationSettings' },
+    { title: 'Scheduled Operations', target: '/apex/KimbleOne__ScheduledOperations' },
+    { title: 'Installed Packages', target: '/lightning/setup/ImportedPackage/home' },
+    { title: 'Company Information', target: '/lightning/setup/CompanyProfileInfo/home' },
+    { title: 'Grant LMA', target: '/lightning/settings/personal/GrantLoginAccess/home' },
+    { title: 'Interface Type Dashboard', target: '/apex/KimbleOne__InterfaceTypeDashboard' },
+  ];
 
-  // const targetList = [
-  //   { title: 'Job Administration', target: '/apex/KimbleOne__JobAdministration' },
-  // ];
-
-  // const targetNodes = targetList.map(({ title, target }) => (
-  //   <button
-  //     type='button'
-  //     onClick={() => openNewPage(target)}
-  //   >
-  //     {title}
-  //   </button>
-  // ));
+  const targetNodes = targetList.map(({ title, target }) => (
+    <button
+      type='button'
+      onClick={() => openNewPage(params.domain, target)}
+    >
+      {title}
+    </button>
+  ));
   
   return (
     <div className="pages-layout">
-      <h2>Pages</h2>
-      {/* {targetNodes} */}
+      {targetNodes}
     </div>
   );
 };
