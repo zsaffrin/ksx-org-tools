c747631514b4873a73e56a5727aae080764ae2c0
Zach Saffrin <awesomezach@gmail.com>
2025-10-08 16:21:56 -0400

    Temporarily deactivate changing parts

--- src/components/App/Pages/Pages.tsx
+++ src/components/App/Pages/Pages.tsx
@@ -1,30 +1,30 @@
-import useTabData from '../../../hooks/useTabData';
+// import useTabData from '../../../hooks/useTabData';
 import './Pages.css';
 
 const Pages = () => {
-  const { domain } = useTabData();
+  // const { domain } = useTabData();
 
-  const openNewPage = (targetUrl: string) => {
-    window.open('https://' + domain + targetUrl);
-  };
+  // const openNewPage = (targetUrl: string) => {
+  //   window.open('https://' + domain + targetUrl);
+  // };
 
-  const targetList = [
-    { title: 'Job Administration', target: '/apex/KimbleOne__JobAdministration' },
-  ];
+  // const targetList = [
+  //   { title: 'Job Administration', target: '/apex/KimbleOne__JobAdministration' },
+  // ];
 
-  const targetNodes = targetList.map(({ title, target }) => (
-    <button
-      type='button'
-      onClick={() => openNewPage(target)}
-    >
-      {title}
-    </button>
-  ));
+  // const targetNodes = targetList.map(({ title, target }) => (
+  //   <button
+  //     type='button'
+  //     onClick={() => openNewPage(target)}
+  //   >
+  //     {title}
+  //   </button>
+  // ));
   
   return (
     <div className="pages-layout">
       <h2>Pages</h2>
-      {targetNodes}
+      {/* {targetNodes} */}
     </div>
   );
 };
