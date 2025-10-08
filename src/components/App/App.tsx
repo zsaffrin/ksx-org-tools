29f60b6534468b03e34cebe69cee040538f2b26d
Zach Saffrin <awesomezach@gmail.com>
2025-10-08 12:42:22 -0400

    Just debug for the time being

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,14 @@
-import useUrlParams from '../../hooks/useUrlParams';
+// import useUrlParams from '../../hooks/useUrlParams';
 import Debug from './Debug/Debug';
 import './App.css';
 
 const App = () => {
-  const { isSalesforce } = useUrlParams();
+  // const { isSalesforce } = useUrlParams();
   
   return (
     <div className="app-layout">
       <h1>KSX Org Tools</h1>
-      {
-        isSalesforce ? [
-          <Debug />,
-        ] : (
-          <div>
-            Not a Salesforce URL
-          </div>
-        )
-      }
+      <Debug />
     </div>
   );
 };
