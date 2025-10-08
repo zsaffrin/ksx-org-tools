822a77064ce9f5ffee2f704b37cf8e93609d41f1
Zach Saffrin <awesomezach@gmail.com>
2025-10-08 18:37:58 -0400

    Add record opener and ObjectLinks search

--- src/components/App/Actions/Actions.tsx
+++ src/components/App/Actions/Actions.tsx
@@ -5,12 +5,16 @@ import './Actions.css';
 
 const Actions = () => {
   const [targetRecordId, setTargetRecordId] = useState<string>('');
+  const [objectSearchTerm, setObjectSearchTerm] = useState<string>('');
   const params = useUrlParams();
 
   const showRecordActions = !!params.recordId;
 
   const handleTargetRecordIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTargetRecordId(e.currentTarget.value);
+  };
+  const handleObjectSearchTermChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setObjectSearchTerm(e.currentTarget.value);
   };
 
   return (
@@ -27,7 +31,7 @@ const Actions = () => {
       )}
       <div className="record-opener">
         <div className="record-opener-label">
-          ID
+          Record Id
         </div>
         <input
           id='targetRecordId'
@@ -46,6 +50,20 @@ const Actions = () => {
           onClick={() => openNewPage(params.domain, `/${targetRecordId}?nooverride=1`)}
         >
           Open w/No-override
+        </button>
+      </div>
+      <div className="record-opener">
+        <input
+          id='objectSearchTerm'
+          type='text'
+          value={objectSearchTerm}
+          onChange={handleObjectSearchTermChange}
+        />
+        <button
+          type='button'
+          onClick={() => openNewPage(params.domain, `/apex/KimbleOne__ObjectLinks?f=${objectSearchTerm}`)}
+        >
+          Search ObjectLinks
         </button>
       </div>
     </div>
