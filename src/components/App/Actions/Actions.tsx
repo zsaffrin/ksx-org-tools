3a81169b745eae89bda72152f77fd5b521b5f46c
Zach Saffrin <awesomezach@gmail.com>
2025-10-08 17:23:29 -0400

    Add record opener

--- src/components/App/Actions/Actions.tsx
+++ src/components/App/Actions/Actions.tsx
@@ -1,22 +1,53 @@
+import { useState } from 'react';
 import useUrlParams from '../../../hooks/useUrlParams';
 import { openNewPage } from '../../../utilities';
 import './Actions.css';
 
 const Actions = () => {
+  const [targetRecordId, setTargetRecordId] = useState<string>('');
   const params = useUrlParams();
 
   const showRecordActions = !!params.recordId;
 
+  const handleTargetRecordIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTargetRecordId(e.currentTarget.value);
+  };
+
   return (
     <div className="actions-layout">
       {showRecordActions && (
+        <div>
+          <button
+            type='button'
+            onClick={() => openNewPage(params.domain, `/${params.recordId}?nooverride=1`)}
+          >
+            Open Current Page with No-override
+          </button>
+        </div>
+      )}
+      <div className="record-opener">
+        <div className="record-opener-label">
+          ID
+        </div>
+        <input
+          id='targetRecordId'
+          type='text'
+          value={targetRecordId}
+          onChange={handleTargetRecordIdChange}
+        />
         <button
           type='button'
-          onClick={() => openNewPage(params.domain, `/${params.recordId}?nooverride=1`)}
+          onClick={() => openNewPage(params.domain, `/${targetRecordId}`)}
         >
-          Open Current Page with No-override
+          Open
         </button>
-      )}
+        <button
+          type='button'
+          onClick={() => openNewPage(params.domain, `/${targetRecordId}?nooverride=1`)}
+        >
+          Open w/No-override
+        </button>
+      </div>
     </div>
   );
 };
