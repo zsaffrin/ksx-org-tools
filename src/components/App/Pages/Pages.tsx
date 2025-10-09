1a59fbed8cac9eb7416a36a318bae9dd257a1d94
Zach Saffrin <awesomezach@gmail.com>
2025-10-09 08:06:23 -0400

    Organize targets

--- src/components/App/Pages/Pages.tsx
+++ src/components/App/Pages/Pages.tsx
@@ -5,21 +5,21 @@ import './Pages.css';
 const Pages = () => {
   const params = useUrlParams();
 
-  const targetList = [
-    { title: 'Job Administration', target: '/apex/KimbleOne__JobAdministration' },
+  const targets = [
     { title: 'Apex Jobs', target: '/lightning/setup/AsyncApexJobs/home' },
-    { title: 'Scheduled Jobs', target: '/lightning/setup/ScheduledJobs/home' },
-    { title: 'Event Classes', target: '/lightning/o/KimbleOne__EventClass__c/home' },
-    { title: 'Reference Data', target: '/lightning/n/KimbleOne__ReferenceData' },
-    { title: 'Org-Wide Config', target: '/apex/KimbleOne__ConfigurationSettings' },
-    { title: 'Scheduled Operations', target: '/apex/KimbleOne__ScheduledOperations' },
-    { title: 'Installed Packages', target: '/lightning/setup/ImportedPackage/home' },
     { title: 'Company Information', target: '/lightning/setup/CompanyProfileInfo/home' },
+    { title: 'Event Classes', target: '/lightning/o/KimbleOne__EventClass__c/home' },
     { title: 'Grant LMA', target: '/lightning/settings/personal/GrantLoginAccess/home' },
+    { title: 'Installed Packages', target: '/lightning/setup/ImportedPackage/home' },
     { title: 'Interface Type Dashboard', target: '/apex/KimbleOne__InterfaceTypeDashboard' },
+    { title: 'Job Administration', target: '/apex/KimbleOne__JobAdministration' },
+    { title: 'Org-Wide Config', target: '/apex/KimbleOne__ConfigurationSettings' },
+    { title: 'Reference Data', target: '/lightning/n/KimbleOne__ReferenceData' },
+    { title: 'Scheduled Jobs', target: '/lightning/setup/ScheduledJobs/home' },
+    { title: 'Scheduled Operations', target: '/apex/KimbleOne__ScheduledOperations' },
   ];
 
-  const targetNodes = targetList.map(({ title, target }) => (
+  const targetNodes = targets.map(({ title, target }) => (
     <button
       type='button'
       onClick={() => openNewPage(params.domain, target)}
