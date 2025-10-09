e6347398ef5fa8b5ef3b927ed9639f6dbffdf0f3
Zach Saffrin <awesomezach@gmail.com>
2025-10-09 15:27:09 -0400

    Define APP_VERSION env var

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
       ],
     }),
   ],
+  define: {
+    '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+  }
   // resolve: {
   //   alias: {
   //     '@': path.resolve(__dirname, './src'),
