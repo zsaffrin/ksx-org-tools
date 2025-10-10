8620466d5ede7ec7438cf6d01fc6739a7174cbe6
Zach Saffrin <awesomezach@gmail.com>
2025-10-10 15:35:38 -0400

    Add ability to show debug data

--- src/components/App/Footer/Footer.tsx
+++ src/components/App/Footer/Footer.tsx
@@ -1,18 +1,35 @@
-import { FaGithub } from 'react-icons/fa';
+import { useState } from 'react';
+import useUrlParams from '../../../hooks/useUrlParams';
+import { FaBug, FaGithub } from 'react-icons/fa';
 import { openNewPage } from '../../../utilities';
 import './Footer.css';
 
 const Footer = () => {
+  const [showDebug, setShowDebug] = useState<boolean>(false);
+  const params = useUrlParams();
+  
   const version = __APP_VERSION__;
   
   return (
     <div className='footer-layout'>
-      <FaGithub className='icon' onClick={() => openNewPage('github.com', '/zsaffrin/ksx-org-tools')} />
-      <div className='info'>
-        <a onClick={() => openNewPage('github.com', '/zsaffrin/ksx-org-tools')}>
-          {`v${version}`}
-        </a>
+      <div className='footer-action-row'>
+        <FaGithub className='icon' onClick={() => openNewPage('github.com', '/zsaffrin/ksx-org-tools')} />
+        <div className='info'>
+          <a onClick={() => openNewPage('github.com', '/zsaffrin/ksx-org-tools')}>
+            {`v${version}`}
+          </a>
+        </div>
+        <FaBug
+          className={'icon' + (showDebug ? ' debugOpen' : ' debugClosed')}
+          onClick={() => setShowDebug(!showDebug)}
+          title='Show debug data'
+        />
       </div>
+      {showDebug && (
+        <div className='footer-debug'>
+          <pre>{JSON.stringify(params, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 };
