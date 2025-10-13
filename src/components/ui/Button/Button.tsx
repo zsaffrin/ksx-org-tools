1eda28c1e7bf55d0bc3d25c823a79c9cf6aeb727
Zach Saffrin <awesomezach@gmail.com>
2025-10-13 13:32:11 -0400

    Add a small size

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -3,14 +3,22 @@ import './Button.css';
 interface ButtonProps {
   /** The text to display inside the Button */
   title?: string | null,
+  /** The size of the button */
+  size?: string | null,
   /** The action to take when the Button is clicked */
-  action?: () => boolean | void,
+  action?: () => null | void,
 }
 
-const Button = ({ title, action }: ButtonProps) => {
+const Button = ({ size, title, action }: ButtonProps) => {
+  const buttonClasses: string[] = ['button'];
+
+  if (size == 'small') {
+    buttonClasses.push('button-small');
+  }
+  
   return (
     <button
-      className='button'
+      className={buttonClasses.join(' ')}
       type='button'
       onClick={action}
     >
