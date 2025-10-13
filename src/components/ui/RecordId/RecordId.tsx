9ef5e90a37b8976fc9cfe69376d7d10bd81903a4
Zach Saffrin <awesomezach@gmail.com>
2025-10-13 13:32:34 -0400

    Add contextual opener buttons

--- src/components/ui/RecordId/RecordId.tsx
+++ src/components/ui/RecordId/RecordId.tsx
@@ -1,19 +1,31 @@
+import useUrlParams from '../../../hooks/useUrlParams';
 import Button from '../Button/Button';
+import { isSalesforceRecordId, openNewPage } from '../../../utilities';
 import './RecordId.css';
 
 interface RecordIdProps {
   recordId?: string | null,
-  showOpenerActions?: boolean | null;
+  showOpenerActions?: boolean;
 }
 
-const RecordId = ({ recordId, showOpenerActions }: RecordIdProps) => {
+const RecordId = ({ recordId, showOpenerActions = true }: RecordIdProps) => {
+  const params = useUrlParams();
+  
   return (
     <div className='record-id-layout'>
       {recordId}
-      {showOpenerActions && (
+      {isSalesforceRecordId(recordId || null) && showOpenerActions && (
         <>
-          <Button title='Open in New' />
-          <Button title='Open no-override' />
+          <Button
+            size='small'
+            title='Open in New'
+            action={() => openNewPage(params.domain, `/${recordId}`)}
+          />
+          <Button
+            size='small'
+            title='Open w/ no-override'
+            action={() => openNewPage(params.domain, `/${recordId}?nooverride=1`)}
+          />
         </>
       )}
     </div>
