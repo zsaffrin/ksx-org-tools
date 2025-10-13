72611d2c1035f8b5bee4b7e772c9a705cdb8fc92
Zach Saffrin <awesomezach@gmail.com>
2025-10-13 17:07:00 -0400

    Use useNav hook instead of utility functions

--- src/components/ui/RecordId/RecordId.tsx
+++ src/components/ui/RecordId/RecordId.tsx
@@ -1,30 +1,39 @@
-import useUrlParams from '../../../hooks/useUrlParams';
 import Button from '../Button/Button';
-import { isSalesforceRecordId, openNewPage } from '../../../utilities';
+import { isSalesforceRecordId } from '../../../utilities';
+import { useNav } from '../../../hooks';
 import './RecordId.css';
 
 interface RecordIdProps {
   recordId?: string | null,
+  sObject?: string | null,
   showOpenerActions?: boolean;
 }
 
-const RecordId = ({ recordId, showOpenerActions = true }: RecordIdProps) => {
-  const params = useUrlParams();
+const RecordId = ({ recordId, sObject, showOpenerActions = true }: RecordIdProps) => {
+  const { openNew } = useNav();
+
+  const isSalesforceId = isSalesforceRecordId(recordId || null);
+
+  const showActions = isSalesforceId && showOpenerActions;
+
+  const urlPath = sObject 
+    ? `lightning/r/${sObject}/${recordId}/view`
+    : `lightning/_classic/${recordId}`;
   
   return (
     <div className='record-id-layout'>
       {recordId}
-      {isSalesforceRecordId(recordId || null) && showOpenerActions && (
+      {showActions && (
         <>
           <Button
             size='small'
             title='Open in New'
-            action={() => openNewPage(params.domain, `/${recordId}`)}
+            action={() => openNew(urlPath)}
           />
           <Button
             size='small'
             title='Open w/ no-override'
-            action={() => openNewPage(params.domain, `/${recordId}?nooverride=1`)}
+            action={() => openNew(urlPath, { nooverride: true })}
           />
         </>
       )}
