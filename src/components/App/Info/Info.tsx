2a0704a2a0d6189ebaeafcab9c90631f839b6bf0
Zach Saffrin <awesomezach@gmail.com>
2025-10-13 11:26:42 -0400

    Trim old comments

--- src/components/App/Info/Info.tsx
+++ src/components/App/Info/Info.tsx
@@ -1,4 +1,3 @@
-// import { ReactNode } from 'react';
 import { ReactNode } from 'react';
 import { FaDatabase, FaFile } from 'react-icons/fa';
 import { AiFillLayout } from 'react-icons/ai';
@@ -16,46 +15,6 @@ const Info = () => {
   const params = useUrlParams();
 
   const facts: InfoFact[] = [];
-
-  // const facts: {
-  //   label?: string | null,
-  //   content?: string | ReactNode | null,
-  // }[] = [
-  //   { label: 'Page Type', content: params.pageType },
-  // ];
-  // if (params.pageType == 'object') {
-  //   facts.push({ label: 'Object', content: params.sObject });
-  // }
-  // if (params.pageType == 'record') {
-  //   facts.push({ label: 'Object', content: params.sObject });
-  //   facts.push({ label: 'Record Id', content: params.recordId });
-  // }
-  // if (params.pageType == 'apex') {
-  //   facts.push({ label: 'Apex Page', content: params.apexPage });
-  //   facts.push({ label: 'Page Args', content: (
-  //     <div className="info-subfacts">
-  //       {params?.apexArgs && Object.keys(params.apexArgs).map((paramKey) => (
-  //         <>
-  //           <div className="info-subfact-label">{paramKey}</div>
-  //           <div className="info-subfact-content">{params.apexArgs && params.apexArgs[paramKey]}</div>
-  //         </>
-  //       ))}
-  //     </div>
-  //   ) });
-  // }
-
-  // return (
-  //   <div className="info-layout">
-  //     <div className="info-facts">
-  //     {facts.map(({ label, content }) => (
-  //       <>
-  //         <div className="info-fact-label">{label}</div>
-  //         <div className="info-fact-content">{content}</div>
-  //       </>
-  //     ))}
-  //     </div>
-  //   </div>
-  // );
 
   if (params.pageType == 'object') {
     facts.push({
