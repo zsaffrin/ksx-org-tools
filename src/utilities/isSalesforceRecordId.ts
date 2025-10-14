65b77ed042430bd53d7cbb43b8e88cdbf2c7eaf8
Zach Saffrin <awesomezach@gmail.com>
2025-10-14 15:19:47 -0400

    Remove debug

--- src/utilities/isSalesforceRecordId.ts
+++ src/utilities/isSalesforceRecordId.ts
@@ -1,6 +1,4 @@
 const isSalesforceRecordId = (stringToEvaluate: string | null) => {
-  console.log('isSalesforceRecordId evaluating ' + stringToEvaluate);
-
   const isCorrectLength = stringToEvaluate?.length == 15 || stringToEvaluate?.length == 18 || false;
 
   if (isCorrectLength) {
