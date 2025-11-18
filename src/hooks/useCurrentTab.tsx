01f8ed9fcd2b0e3d5b7b32ee45ffe87239d81929
Zach Saffrin <awesomezach@gmail.com>
2025-11-18 09:52:17 -0500

    Create hook for current tab data

---
+++ src/hooks/useCurrentTab.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+interface ChromeTabData {
+  url?: string | null,
+};
+
+const useCurrentTab = () => {
+  const [currentTabData, setCurrentTabData] = useState<ChromeTabData | void>({});
+  
+  useEffect(() => {
+    if (!currentTabData?.url || currentTabData.url.length < 1) {
+      refreshTabData();
+    }
+  }, [currentTabData]);
+
+  const refreshTabData = async () => {
+    if (chrome && chrome.tabs) {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (!tabs || tabs.length < 1) {
+          return;
+        }
+        
+        const tabData = tabs[0];
+        setCurrentTabData(tabData);
+      });
+    }
+  };
+  
+  return currentTabData;
+};
+
+export default useCurrentTab;
