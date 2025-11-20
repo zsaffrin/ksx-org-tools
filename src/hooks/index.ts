25b13057a40f2183efe2ccf89cbbefbb36f76f07
Zach Saffrin <awesomezach@gmail.com>
2025-11-20 15:23:17 -0500

    Create hook to access AppState context

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,10 @@
+import useAppState from './useAppState';
 import useCurrentTab from './useCurrentTab';
 import useNav from './useNav';
 import useUrlParams from './useUrlParams';
 
 export {
+  useAppState,
   useCurrentTab,
   useNav,
   useUrlParams,
