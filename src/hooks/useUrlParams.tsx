753db65b2d6b96a010ba8c6827b63f5eefffad03
Zach Saffrin <awesomezach@gmail.com>
2025-11-20 16:12:42 -0500

    Don't need separate properties for this

--- src/hooks/useUrlParams.tsx
+++ src/hooks/useUrlParams.tsx
@@ -15,7 +15,6 @@ interface ApexArgs {
 interface ParamData {
   apexArgs?: ApexArgs | null,
   apexPage?: string | null,
-  apexPageName?: string | null,
   baseUrl?: string | null,
   domain?: string | null,
   domainName?: string | null,
@@ -28,7 +27,6 @@ interface ParamData {
   protocol?: string | null,
   recordId?: string | null,
   sObject?: string | null,
-  sObjectName?: string | null,
   url?: string | null,
 };
 
@@ -87,13 +85,11 @@ const getParamsFromUrl = (url?: string | null) => {
       if (urlParts[4] == 'o') {
         paramData.pageType = 'object';
         paramData.sObject = urlParts[5];
-        paramData.sObjectName = paramData.sObject.replace('KimbleOne__', '').replace('__c', '');
       }
       if (urlParts[4] == 'r') {
         paramData.pageType = 'record';
         paramData.recordId = urlParts[6];
         paramData.sObject = urlParts[5];
-        paramData.sObjectName = paramData.sObject.replace('KimbleOne__', '').replace('__c', '');
       }
       if (urlParts[4] == 'setup') {
         paramData.pageType = 'setup';
@@ -110,7 +106,6 @@ const getParamsFromUrl = (url?: string | null) => {
           paramData.pageType = 'apex';
           const apexParts = oneAddressParts[4].split('?');
           paramData.apexPage = apexParts[0];
-          paramData.apexPageName = paramData.apexPage.replace('KimbleOne__', '');
           paramData.apexArgs = apexParts[1]?.split('&').reduce<ApexArgs>((acc, arg) => {
             const argParts = arg.split('=');
             return ({
@@ -128,7 +123,6 @@ const getParamsFromUrl = (url?: string | null) => {
       paramData.pageType = 'apex';
       const apexParts = urlParts[4].split('?');
       paramData.apexPage = apexParts[0];
-      paramData.apexPageName = paramData.apexPage.replace('KimbleOne__', '');
       paramData.apexArgs = apexParts[1]?.split('&').reduce<ApexArgs>((acc, arg) => {
         const argParts = arg.split('=');
         return ({
