59f49cf9b67543e47125f51b3379f7c7343eaa41
Zach Saffrin <awesomezach@gmail.com>
2025-11-21 15:24:40 -0500

    Add debug log copier

--- src/components/App/Actions/ContextActions.tsx
+++ src/components/App/Actions/ContextActions.tsx
@@ -1,10 +1,14 @@
-import { useNav } from '../../../hooks';
+import { useState } from 'react';
+import { useCurrentTab, useNav } from '../../../hooks';
 import { Button } from '../../ui';
+import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
+import './ContextActions.css';
 
 interface ContextActionsProps {
   params?: {
     baseUrl?: string | null,
     domain?: string | null,
+    pageName?: string | null,
     pageType?: string | null,
     sObject?: string | null,
     recordId?: string | null,
@@ -13,8 +17,15 @@ interface ContextActionsProps {
   }
 }
 
+interface LogCopyResult {
+  type?: string,
+  content?: string | null,
+}
+
 const ContextActions = ({ params }: ContextActionsProps) => {
+  const currentTab = useCurrentTab();
   const { navigate } = useNav();
+  const [logCopyResult, setLogCopyResult] = useState<LogCopyResult | null>(null);
   
   // Job Administration > Pending Jobs runner
   if (params?.apexPage == 'JobsPending') {
@@ -65,6 +76,73 @@ const ContextActions = ({ params }: ContextActionsProps) => {
         />
       </div>
     );
+  }
+
+  // Setup pages
+  if (params?.pageType == 'setup') {
+    // Debug Log Detail
+    if (params.pageName == 'ApexDebugLogDetail') {
+      const getLogContent = async () => {
+        const frame = document.querySelector('iframe');
+        if (frame) {
+          return {
+            type: 'success',
+            content: frame.contentWindow?.document.querySelector('pre.codeBlock')?.textContent,
+          };
+        } else {
+          return {
+            type: 'error',
+            content: 'No iframe found',
+          };
+        }
+      };
+
+      const triggerContentCopy = () => {
+        console.info(currentTab.id);
+        chrome.scripting.executeScript({
+          target: { tabId: currentTab.id || 0 },
+          func: getLogContent,
+        }).then((results) => {
+          const result: LogCopyResult = results[0]?.result || {};
+          if (result.type == 'success') {
+            navigator.clipboard.writeText(result.content || '');
+          }
+          setLogCopyResult(result);
+        });
+      };
+
+      let result;
+      if (logCopyResult) {
+        let classString = 'log-copy-result';
+        let icon;
+        let content = logCopyResult.content;
+        if (logCopyResult.type == 'success') {
+          classString += ' success';
+          icon = <FaCheckCircle />;
+          content = 'Log copied to clipboard';
+        }
+        if (logCopyResult.type == 'error') {
+          classString += ' error';
+          icon = <FaTimesCircle />;
+        }
+        result = (
+          <div className={classString}>
+            {icon}
+            {content}
+          </div>
+        );
+      }
+      
+      return (
+        <div className='log-copy'>
+          <Button
+            title='Copy Log Content'
+            action={triggerContentCopy}
+          />
+          {result}
+        </div>
+      );
+    }
   }
 
   return null;
